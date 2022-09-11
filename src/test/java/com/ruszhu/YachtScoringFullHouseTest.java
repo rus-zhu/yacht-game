db28b1653c9a10e2982a2d5b3125e8f32b1352e9
rus-zhu <microlab982@yandex.ru>
2022-09-11 19:33:47 +0500

    Handle Full House only for valid Full House rolls where there needs to be 2 or 3 of each die.

--- src/test/java/com/ruszhu/YachtScoringFullHouseTest.java
+++ src/test/java/com/ruszhu/YachtScoringFullHouseTest.java
@@ -18,7 +18,7 @@ public class YachtScoringFullHouseTest {
                 .isZero();
     }
 
-    @Disabled
+    @Test
     public void rollOf33355ScoresAs19() throws Exception {
         Yacht yacht = new Yacht();
 
@@ -26,6 +26,16 @@ public class YachtScoringFullHouseTest {
 
         assertThat(score)
                 .isEqualTo(3 + 3 + 3 + 5 + 5);
+    }
+
+    @Test
+    public void rollOf51111ScoresAs0() throws Exception {
+        Yacht yacht = new Yacht();
+
+        int score = yacht.scoreAsFullHouse(List.of(5, 1, 1, 1, 1));
+
+        assertThat(score)
+                .isZero();
     }
 
     @Disabled
